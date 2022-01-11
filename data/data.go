4efccad2f113f8f7a232fc01421073ebb6e7aa04
hujimori <hujimori16@gmail.com>
2022-01-11 21:00:17 +0900

    chapter2途中

---
+++ data/data.go
@@ -0,0 +1,42 @@
+package data
+
+import (
+	"crypto/sha1"
+	"database/sql"
+	"fmt"
+	"log"
+	"math/rand"
+
+	_ "github.com/lib/pq"
+)
+
+var Db *sql.DB
+
+func init() {
+	var err error
+	Db, err = sql.Open("postgres", "dbname=chitChat sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+// create a random UUID with from RFX 4122
+func createUUID() (uuid string) {
+	u := new([16]byte)
+	_, err := rand.Read(u[:])
+	if err != nil {
+		log.Fatalln("Cannnot generate UUID", err)
+	}
+
+	u[8] = (u[8] | 0x40) & 0x7F
+	u[6] = (u[6] & 0xF) | (0x4 << 4)
+	uuid = fmt.Sprintf("%x-%x-%x-%x-%x", u[0:4], u[4:6], u[6:8], u{8: 10}, u[10:])
+	return
+}
+
+// hash plaintext with SHA-1
+func Encrypt(plaintext string) (crypttext string) {
+	crypttext = fmt.Sprintf("%x", sha1.Sum([]byte(plaintext)))
+	return
+}
--- data/user.go
+++ data/user.go
@@ -7,10 +7,59 @@ import (
 type User struct {
 	Id        int
 	Uuid      string
-	name      string
-	email     int
-	password  string
+	Name      string
+	Email     int
+	Password  string
 	CreatedAt time.Time
+}
+
+type Session struct {
+	Id        int
+	Uuid      string
+	Email     string
+	UserId    int
+	CreatedAt time.Time
+}
+
+func (user *User) CreateSession() (session Session, err error) {
+	statement := "insert into sessions (uuid, email, user_id, created_at) values ($1, $2, $3, $4) returning id, uuid, email, user_id, creatd_at"
+	stmt, err := Db.Prepare(statement)
+	if err != nill {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(createUUID(), user.Email, user.Id, time.Now()).Scan(&session.Id, &session.Uuid, &session.Email, &session.UserId, &session.CreatedAt)
+	return
+}
+
+func (user *User) Session() (session Session, err error) {
+	session = Session{}
+	err = Db.QueryRow("SELECT id, uuid, email, user_id, created_at FROM sessions WHERE user_id $1", user_id).Scan(&session.Id, &session.Uuid, &session.Email, &session.UserId, &session.CreatedAt)
+	return
+}
+
+func (session *Session) Check() (valid bool, err error) {
+	err = Db.QueryRow("SELECT id, uuid, email, user_id, created_at, FROM sessions WHERE uuid = $1", session.Uuid).Scan(&session.Id, &session.Uuid, &session.Email, &session.UserId, &session.CreatedAt).Scan(&session.Id, &session.Uuid, &session.Email, &session.UserId, &session.CreatedAt)
+
+	if err != nil {
+		valid = false
+		return
+	}
+	if session.Id != 0 {
+		valid = true
+	}
+	return
+}
+
+func (session *Session) DeleteByUUID() (err error) {
+	statement := "delete from sessions where uuid = $1"
+	stmt, err := Db.Prepare((statement))
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(session.Uuid)
+	return
 }
 
 func Users() (users []User, err error) {
@@ -28,6 +77,49 @@ func Users() (users []User, err error) {
 		users = append(users, user)
 	}
 	rows.Close()
+	return
+}
+
+func SessionDeleteAll() (err error) {
+	statement := "delete from sessions"
+	_, err = Db.Exec(statement)
+	return
+}
+
+func (user *User) Create() (err error) {
+	statement := "insert into users (uuid, name, email, password, created_at) values ($1, $2, $3, $4, $5) returning id, uuid, created_at"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	err = stmt.QueryRow(createUUID(), user.Name, user.Email, Encrypt(user.Password), time.Now()).Scan((&user.Id, &user.Uuid, &user.CreatedAt))
+	return
+}
+
+func (user *User) Delete() (err error) {
+	statement := "delete from users = $1"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(user.Id)
+	return
+	
+}
+
+func (user *User) Update() (err error) {
+	statement := "update users set name = $2, email = $3 where id = $1"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	
+	_, err = stmt.Exec(user.Id, user.Name, user.Email)
 	return
 }
 
